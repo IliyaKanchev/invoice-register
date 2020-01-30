e0907abcf111ce67dc95b268507bc7e5896e6710
Iliya Kanchev <iliqtk@gmail.com>
2020-01-30 13:36:36 +0200

    - added data seed  - added some nugets

--- IvoiceRegister/InvoiceRegisterServer/Code/Models.cs
+++ IvoiceRegister/InvoiceRegisterServer/Code/Models.cs
@@ -10,21 +10,25 @@ namespace InvoiceRegisterServer.Code
         private int _number;
         private DateTime _date;
         private string _description;
-        private float _sum;
+        private double _sum;
 
         private int _clientId;
         private Client _client;
 
-
         public Invoice()
         {
+            _id = 0;
+            _number = 0;
+            _date = DateTime.Now;
+            _description = "";
+            _sum = 0.0;
         }
 
         public int Id { get => _id; set => _id = value; }
         public int Number { get => _number; set => _number = value; }
         public DateTime Date { get => _date; set => _date = value; }
         public string Description { get => _description; set => _description = value; }
-        public float Sum { get => _sum; set => _sum = value; }
+        public double Sum { get => _sum; set => _sum = value; }
         public int ClientId { get => _clientId; set => _clientId = value; }
         public Client Client { get => _client; set => _client = value; }
     }
@@ -38,6 +42,8 @@ namespace InvoiceRegisterServer.Code
 
         public Client()
         {
+            _id = 0;
+            _name = "";
         }
 
         public int Id { get => _id; set => _id = value; }
@@ -58,5 +64,38 @@ namespace InvoiceRegisterServer.Code
 
         public DbSet<Client> Clients { get => _clients; set => _clients = value; }
         public DbSet<Invoice> Invoices { get => _invoices; set => _invoices = value; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // initial seed
+
+            // clients seed
+            Client client1 = new Client() { Id = 1, Name = "Client1" };
+            Client client2 = new Client() { Id = 2, Name = "Client2" };
+
+            modelBuilder.Entity<Client>().HasData(client1);
+            modelBuilder.Entity<Client>().HasData(client2);
+
+            //invoices seed
+            Invoice invoice1 = new Invoice { Id = 1, ClientId = client1.Id, Number = 1, Description = "description 1", Sum = 100.0 };
+            Invoice invoice2 = new Invoice { Id = 2, ClientId = client1.Id, Number = 2, Description = "description 2", Sum = 200.0 };
+            Invoice invoice3 = new Invoice { Id = 3, ClientId = client2.Id, Number = 3, Description = "description 3", Sum = 300.0 };
+            Invoice invoice4 = new Invoice { Id = 4, ClientId = client2.Id, Number = 4, Description = "description 4", Sum = 400.0 };
+
+            modelBuilder.Entity<Invoice>().HasData(invoice1);
+            modelBuilder.Entity<Invoice>().HasData(invoice2);
+            modelBuilder.Entity<Invoice>().HasData(invoice3);
+            modelBuilder.Entity<Invoice>().HasData(invoice4);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=127.0.0.1,1433;Database=test;User Id=SA;Password=TEST_p@ssword1;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
