093b353b120da88fa366ddbc2e28c181c8f7054c
Iliya Kanchev <iliqtk@gmail.com>
2020-01-30 11:53:37 +0200

    - updated nugets

--- IvoiceRegister/InvoiceRegisterServer/Startup.cs
+++ IvoiceRegister/InvoiceRegisterServer/Startup.cs
@@ -29,13 +29,18 @@ namespace InvoiceRegisterServer
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddLogging(opt =>
+            {
+                opt.AddDebug();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            //loggerFactory.AddDebug();
 
             app.UseMvc();
         }
