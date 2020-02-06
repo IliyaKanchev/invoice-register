719d2f03052c3173ecf9cd401f32e72b394b0402
Iliya Kanchev <iliqtk@gmail.com>
2020-02-06 13:05:56 +0200

    - small JS re-factor

--- IvoiceRegister/InvoiceRegisterClient/wwwroot/js/site.js
+++ IvoiceRegister/InvoiceRegisterClient/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function initDateTimePicker(id) {
+    var picker = $(id);
+            
+    picker.datetimepicker();
+        
+    // programmatically clearing of default model value for aestetics
+    if (picker.val() == "0001.01.01 00:00") picker.val("");
+};
